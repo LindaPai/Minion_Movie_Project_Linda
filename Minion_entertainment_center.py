ca47c90fb6da034dc5d7f9ff346f628e129981a2
mrdonnee <kongtinent@gmail.com>
2019-05-30 10:52:15 -0700

    Update Minion_entertainment_center.py

--- Minion_entertainment_center.py
+++ Minion_entertainment_center.py
@@ -5,9 +5,9 @@ despicable_me = movie.Movie(movie_title = "Despicable Me", movie_storyline = "*F
 poster_image = "https://m.media-amazon.com/images/M/MV5BMTY3NjY0MTQ0Nl5BMl5BanBnXkFtZTcwMzQ2MTc0Mw@@._V1_UX182_CR0,0,182,268_AL_.jpg",
 trailer_youtube = "*FILL_IN_TRAILER*")  
 
-despicable_me_2 = movie.Movie("Despicable Me 2", "*FILL_IN_STORYLINE*",
-"https://m.media-amazon.com/images/M/MV5BMjExNjAyNTcyMF5BMl5BanBnXkFtZTgwODQzMjQ3MDE@._V1_UX182_CR0,0,182,268_AL_.jpg",
-"*FILL_IN_TRAILER*")
+despicable_me_2 = movie.Movie(movie_title = "Despicable Me 2", movie_sotryline = "*FILL_IN_STORYLINE*",
+poster_image = "https://m.media-amazon.com/images/M/MV5BMjExNjAyNTcyMF5BMl5BanBnXkFtZTgwODQzMjQ3MDE@._V1_UX182_CR0,0,182,268_AL_.jpg",
+trailer_youtube = "*FILL_IN_TRAILER*")
 
 
 movies = [despicable_me, despicable_me_2 ]
